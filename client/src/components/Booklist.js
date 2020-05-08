a5279e4d8e4c164546ffbfa3444895d57f2d367e
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-05-08 14:28:43 +0400

    setup apollo client in react app

---
+++ client/src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookList = () => {
+  return (
+    <div>
+        <ul id="book-list">
+            <li>   
+                <li>Book name</li>
+            </li>
+        </ul>
+    </div>
+  );
+}
+
+export default BookList;
